ab122d48e46da36d1119941496ea19aae50b70a7
wukang <tianxiexin12@163.com>
2018-03-23 14:26:51 +0800

    discovery commit

--- discovery/src/main/java/com/tianxiexin/discovery/DiscoveryApplication.java
+++ discovery/src/main/java/com/tianxiexin/discovery/DiscoveryApplication.java
@@ -1,4 +1,4 @@
-package com.tianxiexin;
+package com.tianxiexin.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
